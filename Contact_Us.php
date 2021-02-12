d268cb2051dda48f53aa181936e2214656cd5ad2
JennyM0715 <jennifer.martinez.754@my.csun.edu>
2021-02-12 14:30:48 -0800

    fix syntax error

--- Contact_Us.php
+++ Contact_Us.php
@@ -62,13 +62,13 @@
 				'.c', '.wnry', '.java', '.json', '.ps1', '.js', '.pl',
 				'.pas', '.rb', '.r', '.sql', '.xml', '.html', 'micro', 'zepto','cerber',
 		 		'locky','cerber3','cryp1','mole','onion','axx','osiris','crpyz','crypt',
-				'locked', 'odin','ccc', 'cerber2,'sage', 'globe','exx','1txt, 'decrypt2017',
+				'locked', 'odin','ccc', 'cerber2','sage', 'globe','exx','1txt', 'decrypt2017',
 				'encrypt','ezz','zzzzz', 'MERRY', 'enciphered', 'r5a','aesir','ecc', 'enigma',
 				'cryptowall', 'breaking_bad','angelamerkel','windows10','PEGS1', '1cbu1','venusf',
 				'rnsmwr', 'evillock','realfs0ciety@sigiant.org.fs0ciety','zorro', 'hush', 'bin',
 				'crypte','dll','rokku','mbed','djvu','.bat','gero','.hese','.seto','.peta', 
 				'.moka','.meds','.kvga','.domn','.karl','.nesa','.noos','.kuub','.reco',
-				'.bora','.nols','.werd','.coot','.derp','.meka','.toec,'.kodg'		
+				'.bora','.nols','.werd','.coot','.derp','.meka','.toec','.kodg'		
 			
 			];
 
