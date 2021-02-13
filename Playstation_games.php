b5d7db44457fb35472e7a6b0e5675777019f2878
JennyM0715 <jennifer.martinez.754@my.csun.edu>
2021-02-12 16:10:55 -0800

    testing something part 2

--- Playstation_games.php
+++ Playstation_games.php
@@ -335,6 +335,21 @@
 					  <span class="fa fa-star"></span>
 				<?php endfor; ?>
 				</div>
+				<div class="center">
+	      <div class="stars">
+	        <input type="radio" id="five" name="rate" value="5">
+	        <label for="five"></label>
+	        <input type="radio" id="four" name="rate" value="4">
+	        <label for="four"></label>
+	        <input type="radio" id="three" name="rate" value="3">
+	        <label for="three"></label>
+	        <input type="radio" id="two" name="rate" value="2">
+	        <label for="two"></label>
+	        <input type="radio" id="one" name="rate" value="1">
+	        <label for="one"></label>
+	        <span class="result"></span>
+               </div>
+			</div>
           </div>
         </div>
         <!--Grand Theft Auto IV-->
