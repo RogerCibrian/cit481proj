48b5cdcecf131e3b5cdb51b34c18a125c8ee883c
JennyM0715 <jennifer.martinez.754@my.csun.edu>
2021-02-12 16:15:08 -0800

    stars styles sheet?

--- Playstation_games.php
+++ Playstation_games.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>RottenPotatoes</title>
     <link rel="stylesheet" href="main.css" />
+	<link rel="stylesheet" href="../style2.css" />
     <link
       rel="stylesheet"
       href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"
