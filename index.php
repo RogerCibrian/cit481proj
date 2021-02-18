a848d1eb8737108281e10afcd8cdd783d0c5fa3f
Jose <jose.hernandez.827@my.csun.edu>
2021-02-17 19:29:08 -0800

    adding source for fontawesome icons, to add twitter icon to navbar in order to link social media account

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
+	<script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>RottenPotatoes</title>
     <link rel="stylesheet" href="main.css" />
