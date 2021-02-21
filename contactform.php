028631f3784da5338488184b58a7ac5f8e2be3f2
RogerCibrian <70341939+RogerCibrian@users.noreply.github.com>
2021-02-20 23:49:17 -0800

    debug enable pt 2

--- contactform.php
+++ contactform.php
@@ -62,10 +62,10 @@ if(isset($_POST['submit'])){ /*checks whether user submitted form*/
 
 	if(!$mail->Send()){
 //	use these echos for debugging, comment out page redirect
-	//echo "message not sent: " . $mail->ErrorInfo;} 
-	//else echo "message sent";}
-	header("location:/Contact_Us.php?msg=err");}
-	else{
-	header("location:/Contact_Us.php?msg=suc");}}
+	echo "message not sent: " . $mail->ErrorInfo;} 
+	else echo "message sent";}
+	//header("location:/Contact_Us.php?msg=err");}
+	//else{
+	//header("location:/Contact_Us.php?msg=suc");}}
 //	$mail->ClearAddresses();}
 ?>
