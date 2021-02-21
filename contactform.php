d26b5eb7454d61c47cf13df693afe806922275e0
RogerCibrian <70341939+RogerCibrian@users.noreply.github.com>
2021-02-21 00:17:54 -0800

    parsing ini file

--- contactform.php
+++ contactform.php
@@ -38,6 +38,7 @@ if(isset($_POST['submit'])){ /*checks whether user submitted form*/
 	$mail->SMTPAuth = true;
 	$mail->AuthType='LOGIN';
 	require '/var/app/login.ini';
+	$login = parse_ini_file('/var/app/login.ini', true);
 //	$mail->Username = ''; Saving data in file outside of github and root directory to prevent unauthed access to email
 //	$mail->Password = '';
 	$mail->Port = 587;
