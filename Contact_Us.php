013a2fca4e7ca1f312248ffddd54675795fc6521
JennyM0715 <jennifer.martinez.754@my.csun.edu>
2021-02-22 21:24:27 -0800

    the %vh is not a unit of measure in main.css in side body{min-height}

--- Contact_Us.php
+++ Contact_Us.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>RottenPotatoes</title>
-    <link rel="stylesheet" href="/main.css" />
+    <link rel="stylesheet" href="main.css" />
     <link
       rel="stylesheet"
       href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"
