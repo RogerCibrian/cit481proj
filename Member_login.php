2f9ba172c481868ae3b2a5a4dc3829a12119f514
JennyM0715 <jennifer.martinez.754@my.csun.edu>
2021-02-22 16:00:32 -0800

    trying to edit file via Git Bash

--- Member_login.php
+++ Member_login.php
@@ -89,12 +89,12 @@
       /* Responsive layout - when the screen is less than 600px wide, 
       make the two columns stack on top of each other instead of next 
       to each other */
-      @media screen and (max-width: 600px) {
-        input[type="submit"] {
+     /* @media screen and (max-width: 600px) {*/
+       /* input[type="submit"] {
           width: 100%;
-          margin-top: 0;
-        }
-      }
+          margin-top: 0;*/
+       /* }*/
+     /*}*/
     </style>
   </head>
 
