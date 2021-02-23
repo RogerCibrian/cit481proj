e30838a64169600d320fa8d4f222c993be496d10
JennyM0715 <jennifer.martinez.754@my.csun.edu>
2021-02-22 19:29:59 -0800

    edit between show passwrd and forgot password fields

--- Member_login.php
+++ Member_login.php
@@ -134,6 +134,7 @@
           placeholder="Enter password"
 	  required>
 	<input type="checkbox" onclick="myFunction()" style="float: left"><p style="float: left; text-indent: 5px;"> Show Password</p>
+        <br />
         <ol>
           <span class="psw" style="margin-right: 40px"
             >Forgot <a href="Passwrd_recovery.php">password?</a></span
