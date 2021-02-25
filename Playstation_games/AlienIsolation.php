1939286bfbbe5ca8b5f3d235b522859505d6b6cd
mario-cit <71296920+mario-cit@users.noreply.github.com>
2021-02-24 23:27:38 -0800

    almost

--- Playstation_games/AlienIsolation.php
+++ Playstation_games/AlienIsolation.php
@@ -542,7 +542,7 @@ parent.addEventListener('click', event => {
           //let str = event.target.parentElement.parentElement.previousElementSibling.firstElementChild.innerHTML;
           //get the <h> tag to add to the count summary of the liked event
           let str2 = event.target.parentElement.nextElementSibling;
-          let str3 = event.target.parentElement.parentElement.parentElement.firstElementChild.outerHTML;
+          let str3 = event.target.parentElement.parentElement.parentElement.firstElementChild.innerHTML;
           console.log(str3);
           //start at the closing character of the first span tag
           /*str = str.substring(str.indexOf(">") + 2);
