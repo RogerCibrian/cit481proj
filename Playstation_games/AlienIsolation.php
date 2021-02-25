182fc3b72995f814cdfd6fb6170718838610a48e
mario-cit <71296920+mario-cit@users.noreply.github.com>
2021-02-24 22:21:05 -0800

    sources

--- Playstation_games/AlienIsolation.php
+++ Playstation_games/AlienIsolation.php
@@ -42,7 +42,8 @@ $num = 0;
     rel="stylesheet"
     href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
   />
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://kit.fontawesome.com/961faa2e94.js" crossorigin="anonymous"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   <link rel="stylesheet" href="/main.css" />
   <link rel="stylesheet" href="../style2.css" />
