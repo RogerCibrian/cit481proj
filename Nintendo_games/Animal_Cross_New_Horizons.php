5b0b936d6d56051f7d620bc865d7549929dfce2d
mario-cit <71296920+mario-cit@users.noreply.github.com>
2021-02-26 12:11:03 -0800

    sdf

--- Nintendo_games/Animal_Cross_New_Horizons.php
+++ Nintendo_games/Animal_Cross_New_Horizons.php
@@ -671,7 +671,8 @@ parent.addEventListener('click', event => {
            }
         }
 });          
- $('.userrating-form').submit(function(e){e.preventDefault();
+ $('.userrating-form').submit(function(e){
+   e.preventDefault();
         $.ajax({
             url     : $(this).attr('action'),
             type    : $(this).attr('method'),
