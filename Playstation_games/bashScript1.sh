8736dbe44b09d039be6dcb362e86adab00864f86
mario-cit <71296920+mario-cit@users.noreply.github.com>
2021-02-25 20:55:12 -0800

    sdf

--- Playstation_games/bashScript1.sh
+++ Playstation_games/bashScript1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 for i in *.php; do
-sed -i -e '/3.3.7\/js\/bootstrap.min.js/a\\<script src="https:\/\/kit.fontawesome.com\/961faa2e94.js" crossorigin="anonymous"><\/script>' < $i;
+sed -i -e '/3.3.7\/js\/bootstrap.min.js/a\\<script src="https:\/\/kit.fontawesome.com\/961faa2e94.js" crossorigin="anonymous"><\/script>' $i;
 done
