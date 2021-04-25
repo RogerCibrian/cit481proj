9a6f67f24561dcd237f652305cc6539acbdd790a
RogerCibrian <70341939+RogerCibrian@users.noreply.github.com>
2021-04-24 21:55:12 -0700

    missed another style tag

--- fullnavbar.php
+++ fullnavbar.php
@@ -8,7 +8,7 @@ echo '<link rel="stylesheet" href="./css/fullnavbar.css">';
 echo '<nav class="navbar navbar-inverse">';
 echo     '<div class="container-fluid">';
 echo        '<div class="navbar-header">';
-echo          '<a class="navbar-brand" href="index.php" style="padding: 0;">
+echo          '<a class="navbar-brand" href="index.php">
 				<img src="/images/logo.png" class="fullnavbar_php_logo" alt="Logo">
 			  </a>';
 echo        '</div>';
