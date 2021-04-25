7648507a69d7b1a9834f3cfba5085c5facd52477
Jennifer Martinez <JenniferMartinez0715@yahoo.com>
2021-04-25 13:50:20 -0700

    created external css file

--- PC_games.php
+++ PC_games.php
@@ -15,100 +15,9 @@
     />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-    <style>
-      * {
-        box-sizing: border-box;
-      }
-
-      /* Center website */
-      .main {
-        max-width: 850px;
-        margin: auto;
-      }
-
-      h1 {
-        font-size: 50px;
-        word-break: break-all;
-      }
-
-      .row {
-        margin: 10px -16px;
-      }
-
-      /* Add padding BETWEEN each column */
-      .row,
-      .row > .column {
-        padding: 8px;
-      }
-
-      /* Create three equal columns that floats next to each other */
-      .column {
-        float: left;
-        width: 33.33%;
-        display: none; /* Hide all elements by default */
-      }
-
-      /* Clear floats after rows */
-      .row:after {
-        content: "";
-        display: table;
-        clear: both;
-      }
-
-      /* Content */
-      .content {
-        background-color: #00dbf966;
-        padding: 10px;
-        height: 350px;
-        width: auto;
-      }
-
-      /* The "show" class is added to the filtered elements */
-      .show {
-        display: block;
-      }
-
-      /* Style the buttons */
-      .btn {
-        border: none;
-        outline: none;
-        padding: 12px 16px;
-        background-color: #222222e6;
-		color: white;
-        cursor: pointer;
-        margin-left: 20px;
-      }
-
-      .btn:hover {
-        background-color: #ddd;
-      }
-
-      .btn.active {
-        background-color: #f25bffa1;
-        color: white;
-      }
-      /* scroll to the top button*/
-      #myBtn {
-        display: none;
-        position: fixed;
-        bottom: 20px;
-        right: 10px;
-        z-index: 99;
-        font-size: 18px;
-        border: none;
-        outline: none;
-        background-color: black;
-        color: white;
-        cursor: pointer;
-        padding: 10px;
-        border-radius: 4px;
-        width: 100px;
-      }
-
-      #myBtn:hover {
-        background-color: #555;
-      }
-    </style>
+   
+    <!-------css edits for video game section/category are in external css link----->
+    <link rel="stylesheet" href="../css/PC_games.css"/>
   </head>
 
   <body>
@@ -130,7 +39,7 @@
       <hr />
       <!----scroll to top button-->
       <button onclick="topFunction()" id="myBtn" title="Go to top">Top</button>
-      <h2 style="color: white">Filter</h2>
+      <h2>Filter</h2>
 
       <div id="myBtnContainer">
         <button class="btn active" onclick="filterSelection('all')">
@@ -154,9 +63,10 @@
           <div class="content">
             <a href="/PC_games/TheWitcher3_Wild_Hunt.php ">
               <img
+              class="PC_images"
                 src="/PC_games/220px-Witcher_3_cover_art.jpg"
                 alt="The Witcher 3: Wild Hunt   "
-                style="width: 100%; height: 275px"
+               
             /></a>
             <h4>The Witcher 3: Wild Hunt</h4>
            
@@ -169,7 +79,7 @@
               <img
                 src="/PC_games/220px-Fall_Guys_cover.jpg"
                 alt="Fall Guys: Ultimate Knockout"
-                style="width: 100%; height: 275px"
+                class="PC_images"
             /></a>
             <h4>Fall Guys: Ultimate Knockout</h4>
             
@@ -182,7 +92,7 @@
               <img
                 src="/PC_games/220px-Dark_souls_3_cover_art.jpg"
                 alt="Dark Souls III "
-                style="width: 100%; height: 270px"
+                class="PC_dark_souls_image"
             /></a>
             <h4>Dark Souls III</h4>
             
@@ -195,7 +105,7 @@
               <img
                 src="/PC_games/220px-Control.jpg"
                 alt="Control"
-                style="width: 100%; height: 275px"
+                class="PC_images"
             /></a>
             <h4>Control</h4>
             
@@ -208,7 +118,7 @@
               <img
                 src="/PC_games/220px-Red_Dead_Redemption_II.jpg"
                 alt="Red Dead Redemption 2 "
-                style="width: 100%; height: 275px"
+                class="PC_images"
             /></a>
             <h4>Red Dead Redemption 2</h4>
            
@@ -221,7 +131,7 @@
               <img
                 src="/PC_games/220px-Cover_Art_of_Doom_Eternal.png"
                 alt="Doom: Eternal"
-                style="width: 100%; height: 275px"
+                class="PC_images"
             /></a>
             <h4>Doom: Eternal</h4>
           
@@ -234,7 +144,7 @@
               <img
                 src="/PC_games/220px-Garry_s_Mod_logo.png"
                 alt="Garry's Mod"
-                style="width: 100%; height: 275px"
+                class="PC_images"
             /></a>
             <h4>Garry's Mod</h4>
             
@@ -247,7 +157,7 @@
               <img
                 src="/PC_games/220px-Diablo_III_cover.png"
                 alt="Diablo III "
-                style="width: 100%; height: 275px"
+                class="PC_images"
             /></a>
             <h4>Diablo III</h4>
            
@@ -260,7 +170,7 @@
               <img
                 src="/PC_games/220px-Minecraft_cover.png"
                 alt="Minecraft "
-                style="width: 100%; height: 275px"
+                class="PC_images"
             /></a>
             <h4>Minecraft</h4>
            
@@ -273,7 +183,7 @@
               <img
                 src="/PC_games/220px-WoW_Box_Art1.jpg"
                 alt="World of Warcraft"
-                style="width: 100%; height: 275px"
+                class="PC_images"
             /></a>
             <h4>World of Warcraft</h4>
            
@@ -286,7 +196,7 @@
               <img
                 src="/PC_games/league-of-legends_hu5457919bdd3a081ce5d4f2b2d385ed1a_580546_250x0_resize_q75_catmullrom.jpg"
                 alt="League of Legends"
-                style="width: 100%; height: 275px"
+                class="PC_images"
             /></a>
             <h4>League of Legends</h4>
            
@@ -299,7 +209,7 @@
               <img
                 src="/PC_games/220px-Dying_Light_cover.jpg"
                 alt="Dying Light "
-                style="width: 100%; height: 275px"
+                class="PC_images"
             /></a>
             <h4>Dying Light</h4>
            
