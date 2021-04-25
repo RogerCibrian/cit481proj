0e2469cf137eb32a003a79aba719c1c4ae50fce5
RogerCibrian <70341939+RogerCibrian@users.noreply.github.com>
2021-04-24 22:25:01 -0700

    contact form js to external

--- Contact_Us.php
+++ Contact_Us.php
@@ -21,75 +21,7 @@ session_start();
 	
 	 <!-- Google reCAPTCHA API Call -->
     <script src="https://www.google.com/recaptcha/api.js" async defer></script>
-    <script>
-	function ValidateContactForm(){
-		
-		var full_name_field = document.forms["myForm"]["name"].value;
-		var email_field = document.forms["myForm"]["email"].value;
-		var subject_field = document.forms["myForm"]["subject"].value;
-		var message_box_field = document.forms["myForm"]["message"].value;
-
-
-		if (full_name_field == ""){
-			alert("Full Name field must be filled");
-			return false;
-		}
-		if (email_field == ""){
-			alert("Emaill Address field must be filled in");
-			return false;
-		}
-		if (subject_field == ""){
-			alert("Subject field must be filled in");
-			return false;
-		}	
-		if (message_box_field == ""){
-			alert("Message field must be filled out");
-			return false;
-		}
-	
-		var email = document.getElementById('email_addr');
-		var filter = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-		if (!filter.test(email.value)){
-			alert("Please provide a valid email address. Example: oinkster159@sbcglobal.net");
-			return false;
-		}
-	}
-	
-		/***************************
-	* RESTRICT KEYWORDS IN MESSAGE BOX
-	*8************************8*/
-	$(document).ready(function() {
-		$('#textarea-box').keyup(function() {
-			var textBoxValue = $(this).val();
-			console.log(textBoxValue);
-
-			var forbidden = [
-				'http', 'https', '.py', '.php', '.exe', '.diff', '.sh',
-				'.c', '.wnry', '.java', '.json', '.ps1', '.js', '.pl',
-				'.pas', '.rb', '.r', '.sql', '.xml', '.html', 'micro', 'zepto','cerber',
-		 		'locky','cerber3','cryp1','mole','onion','axx','osiris','crpyz','crypt',
-				'locked', 'odin','ccc', 'cerber2','sage', 'globe','exx','1txt', 'decrypt2017',
-				'encrypt','ezz','zzzzz', 'MERRY', 'enciphered', 'r5a','aesir','ecc', 'enigma',
-				'cryptowall', 'breaking_bad','angelamerkel','windows10','PEGS1', '1cbu1','venusf',
-				'rnsmwr', 'evillock','realfs0ciety@sigiant.org.fs0ciety','zorro', 'hush', 'bin',
-				'crypte','dll','rokku','mbed','djvu','.bat','gero','.hese','.seto','.peta', 
-				'.moka','.meds','.kvga','.domn','.karl','.nesa','.noos','.kuub','.reco',
-				'.bora','.nols','.werd','.coot','.derp','.meka','.toec','.kodg'		
-			
-			];
-
-			for (var i =0; i < forbidden.length; i++){
-				if(textBoxValue.search(forbidden[i]) > -1){
-					textBoxValue = textBoxValue.replace(forbidden[i], '');
-				}
-			}
-			$(this).val(textBoxValue);
-		});
-	});
-	
-
-
-	</script>
+    <script src='./js/validateContact.js'></script>
     <!---Start of CSS style for contact form-->
     <style>
 	/*attempting to fix how it looks on mobile device*/
