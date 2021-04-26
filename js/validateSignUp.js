4c37ee3b995e7e2fbffcd27f490359990c48b742
Jennifer Martinez <JenniferMartinez0715@yahoo.com>
2021-04-26 12:12:10 -0700

    half of the validation func works

--- js/validateSignUp.js
+++ js/validateSignUp.js
@@ -29,13 +29,6 @@ function validateForm(){
     var password1 = document.forms["myForm"]["user_pwd"].value;
     var password2 = document.forms["myForm"]["re_user_pwd"].value;
 
-    /* makes sure if value enetred in email input field is in actual email format*/
-    var email = document.getElementById('email');
-    var filter = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if (!filter.test(email.value)){
-        alert("Please provide a valid email address. Example: oinster159@sbcgobal.net");
-            /*return false;*/
-    }
 
     if ( password1 == "") {
        alert("Password field must be filled out");
@@ -45,6 +38,15 @@ function validateForm(){
        alert("Re-Enter Password field must be filled out");
         return false;
    }
+
+    /* makes sure if value enetred in email input field is in actual email format*/
+    var email = document.getElementById('email');
+    var filter = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!filter.test(email.value)){
+        alert("Please provide a valid email address. Example: oinster159@sbcgobal.net");
+            /*return false;*/
+    }
+    
    // Function to check Whether both passwords 
    // are the same or not. 
    else if (password1 != password2) { 
