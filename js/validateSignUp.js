eb88a2de30f8e06bb729a9c27ee176ff6bfeab5a
Jennifer Martinez <JenniferMartinez0715@yahoo.com>
2021-04-26 12:33:23 -0700

    fixing validation JS

--- js/validateSignUp.js
+++ js/validateSignUp.js
@@ -1,4 +1,28 @@
 
+/* 
+the alert messages does display on the screen for all input fields
+ but the form still runs the signupuser.php I think because if user ignores the 
+alert messages for email validation--> alert("Please provide a valid email 
+address. Example: oinster159@sbcgobal.net"); then the all the info
+entered in the other input fields disappear and the url changes to 
+https://www.rottenpotatoes.org/Sign_Up.php?error=invalidemailaddress
+
+*/
+
+/*
+    When all input fields have value and the information is correct, email 
+    address format is correct, BUT if the user's password and re-enter password
+    values don't match and the google recaptch is clicked on, the alert message 
+    for password match --alert ("\nPassword did not match: Please try again...");---
+    does display but the form submits and creates a new account eventho the passwords
+    don't match. 
+
+    if passwords dont match AND google recaptcha is not click on then the
+    alert message----alert ("\nPassword did not match: Please try again...");--
+    displays, form is not submitted, and the values of the other input fields are
+    still present.
+*/
+
 document.getElementById("sign-up-button").addEventListener("click", validateForm);
 function validateForm(){
     var l = document.forms["myForm"]["username"].value;
@@ -44,7 +68,7 @@ function validateForm(){
     var filter = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (!filter.test(email.value)){
         alert("Please provide a valid email address. Example: oinster159@sbcgobal.net");
-            /*return false;*/
+            return false;
     }
 
    // Function to check Whether both passwords 
@@ -58,7 +82,6 @@ function validateForm(){
     }
 
    
-
-    console.log("works");
 }
+console.log("works");
 
