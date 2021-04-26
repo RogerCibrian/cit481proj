5913b310f883aa2f0337999ec30826f74d6641dc
RogerCibrian <70341939+RogerCibrian@users.noreply.github.com>
2021-04-25 18:07:23 -0700

    remove semi colons

--- js/scroll_top.js
+++ js/scroll_top.js
@@ -6,7 +6,7 @@
  // When the user scrolls down 20px from the top of the document, show the button
  window.onscroll = function () {
    scrollFunction();
- };
+ }
 
  function scrollFunction() {
    if (
