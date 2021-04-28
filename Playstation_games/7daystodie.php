4e43608e214cff13ea80ae00f7c0cacb729c4bbd
mario-cit <71296920+mario-cit@users.noreply.github.com>
2021-04-27 20:25:03 -0700

    change to 7daystodie.php

--- Playstation_games/7daystodie.php
+++ Playstation_games/7daystodie.php
@@ -36,19 +36,19 @@ $num = 0;
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>RottenPotatoes</title>
 	<link rel="stylesheet" href="/main.css" />
-  <script src="https://kit.fontawesome.com/961faa2e94.js" crossorigin="anonymous" data-auto-add-css="false"></script>
-    <link
+  <script src="https://kit.fontawesome.com/961faa2e94.js" crossorigin="anonymous"></script>
+   <!-- <link
     rel="stylesheet"
     href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
     
-  />
+  /> -->
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   
   <link
     rel="stylesheet"
     href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
   />
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+ <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> -->
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
   
