97cb4801b9e168f658ef8b03c2f56d68c6a05844
Jennifer Martinez <JenniferMartinez0715@yahoo.com>
2021-04-27 20:07:07 -0700

    added data-auto-add-css="false" on 7daystodie.php

--- Playstation_games/7daystodie.php
+++ Playstation_games/7daystodie.php
@@ -39,6 +39,7 @@ $num = 0;
     <link
     rel="stylesheet"
     href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+    data-auto-add-css="false"
   />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <link
