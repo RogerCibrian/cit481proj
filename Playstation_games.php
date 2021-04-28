a099844b3a73dd08239daa33c54ff744cb59ded1
Jennifer Martinez <JenniferMartinez0715@yahoo.com>
2021-04-28 09:26:52 -0700

    changed bootstrap min js version

--- Playstation_games.php
+++ Playstation_games.php
@@ -9,8 +9,6 @@
       rel="stylesheet"
       href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"
     />
-
-    
   <!--<link
       rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
@@ -18,7 +16,7 @@
     <link rel="stylesheet" href="../css/fontawesome.min.css" />
     <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>-->
     <!--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>-->
-    <script src="../js/jquery-3.6.0.min.js"></script>
+    -<script src="../js/jquery-3.6.0.min.js"></script>
     <script src="../js/bootstrap.min.js"></script>
 
     
