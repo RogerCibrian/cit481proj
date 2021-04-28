828e507f1130b666ef3b39355de4c49a1fcf36f0
Jennifer Martinez <JenniferMartinez0715@yahoo.com>
2021-04-28 09:29:15 -0700

    type error

--- Playstation_games.php
+++ Playstation_games.php
@@ -7,8 +7,7 @@
     <link rel="stylesheet" href="/main.css" />
     <link
       rel="stylesheet"
-      href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"
-    />
+      href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"/>
   <!--<link
       rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
@@ -16,7 +15,7 @@
     <link rel="stylesheet" href="../css/fontawesome.min.css" />
     <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>-->
     <!--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>-->
-    -<script src="../js/jquery-3.6.0.min.js"></script>
+    <script src="../js/jquery-3.6.0.min.js"></script>
     <script src="../js/bootstrap.min.js"></script>
 
     
@@ -29,7 +28,6 @@
   <?php 
         include 'fullnavbar.php';
   ?>
-
     <!----end of Navigation Bar-->
     
     <!---added by Sean-->
