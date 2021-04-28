9eb8aa51e45213c34b29ac597260e515c8c002c2
Jennifer Martinez <JenniferMartinez0715@yahoo.com>
2021-04-28 09:13:29 -0700

    removed local copy of bootstrap.min.css...would get errors

--- Playstation_games.php
+++ Playstation_games.php
@@ -5,12 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>RottenPotatoes</title>
     <link rel="stylesheet" href="/main.css" />
-    <!--<link
+    <link
       rel="stylesheet"
       href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"
-    />--->
-    <!---bootstrap min css v4.3.1---->
-    <link rel="stylesheet" href="../css/bootstrap.min.css" />
+    />
+
+    
   <!--<link
       rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
