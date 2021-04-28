3192add3f3ecccf0c111c27f70a3a57d9757f5b4
mario-cit <71296920+mario-cit@users.noreply.github.com>
2021-04-28 12:34:08 -0700

    add jquery script tag

--- Playstation_games/7daystodie.php
+++ Playstation_games/7daystodie.php
@@ -47,7 +47,7 @@ $num = 0;
     rel="stylesheet"
     href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
   />
-<script type='text/javascript' src='../jquery-3.6.0.min.js'></script>
+<script type='text/javascript' src='../js/jquery-3.6.0.min.js'></script>
   <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> --> 
   
 <!---- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> ---->
@@ -294,6 +294,6 @@ placeholder="Enter your comment here..."></textarea>
     <!---END OF FOOTER-->
      <!---external JS script: dislike container, likes container, form-container, userform----->
 	  <script type="text/javascript" src='../js/commentSystem.js'></script>
-    <script type="text/javascript" src="/../js/commentSystem.js"></script>
+   
 </body>
 </html>
