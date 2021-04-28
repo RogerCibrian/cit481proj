b0a5b24016326655b7d7a6171fad9d8435087535
Jennifer Martinez <JenniferMartinez0715@yahoo.com>
2021-04-28 11:21:16 -0700

    trying to change position of search bar

--- Playstation_games.php
+++ Playstation_games.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>RottenPotatoes</title>
-    <link rel="stylesheet" href="/main.css" />
+   
    <link
       rel="stylesheet"
       href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"/>
@@ -21,6 +21,7 @@
     
      <!-------css edits for video game section/category are in external css link----->
      <link rel="stylesheet" href="../css/Playstation_games.css"/>
+     <link rel="stylesheet" href="/main.css" />
   </head>
 
   <body>
