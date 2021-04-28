007079779a3c790d608de9605fccf39da16e82ef
eneemz <erik.niemans@gmail.com>
2021-04-27 21:10:19 -0700

    Adding bootstrap script

--- Playstation_games/7daystodie.php
+++ Playstation_games/7daystodie.php
@@ -49,7 +49,9 @@ $num = 0;
     href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
   />
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> 
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  
+<!---- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> ---->
+<script type="text/javascript" src='../js/bootstrap.min.js'></script>
 
   
   <link rel="stylesheet" href="../style2.css" />
