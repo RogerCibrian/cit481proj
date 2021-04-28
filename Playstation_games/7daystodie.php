ad2342e0fdfb6468bc1cc9189f3b3d0c15d10961
Jennifer Martinez <JenniferMartinez0715@yahoo.com>
2021-04-27 20:09:39 -0700

    added data-auto-add-css="false" to script font awesome

--- Playstation_games/7daystodie.php
+++ Playstation_games/7daystodie.php
@@ -39,7 +39,7 @@ $num = 0;
     <link
     rel="stylesheet"
     href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
-    data-auto-add-css="false"
+    
   />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <link
@@ -48,7 +48,7 @@ $num = 0;
   />
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<script src="https://kit.fontawesome.com/961faa2e94.js" crossorigin="anonymous"></script>
+<script src="https://kit.fontawesome.com/961faa2e94.js" crossorigin="anonymous" data-auto-add-css="false"></script>
   
   <link rel="stylesheet" href="../style2.css" />
   
