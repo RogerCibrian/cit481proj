b98e69d4ebb19c12dfb522e6915ce5dbf370a58e
Jennifer Martinez <JenniferMartinez0715@yahoo.com>
2021-04-27 22:48:17 -0700

    <script type="text/javascript" src='../js/commentSystem.js'></script> to all PS games

--- Playstation_games/Agony.php
+++ Playstation_games/Agony.php
@@ -290,7 +290,7 @@ placeholder="Enter your comment here..."></textarea>
     </div>
     <!---END OF FOOTER-->
 
-   <!---external JS script: dislike container, likes container, form-container, userform----->
+   <!---external JS script: dislike container, likes container, form-container,userform----->
    <script type="text/javascript" src='../js/commentSystem.js'></script>
 </body>
 </html>
