e307e342a7f83490487283fc3a77a89c292ca4ef
Jennifer Martinez <JenniferMartinez0715@yahoo.com>
2021-04-27 20:18:31 -0700

    moved script font awesome

--- Playstation_games/7daystodie.php
+++ Playstation_games/7daystodie.php
@@ -36,13 +36,14 @@ $num = 0;
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>RottenPotatoes</title>
 	<link rel="stylesheet" href="/main.css" />
+  <script src="https://kit.fontawesome.com/961faa2e94.js" crossorigin="anonymous" data-auto-add-css="false"></script>
     <link
     rel="stylesheet"
     href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
     
   />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <script src="https://kit.fontawesome.com/961faa2e94.js" crossorigin="anonymous" data-auto-add-css="false"></script>
+  
   <link
     rel="stylesheet"
     href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
