d6124252dcbc3008b412c814d0f70702f6861300
Jennifer Martinez <JenniferMartinez0715@yahoo.com>
2021-04-27 20:16:25 -0700

    moved font awesome script above link

--- Playstation_games/7daystodie.php
+++ Playstation_games/7daystodie.php
@@ -42,13 +42,14 @@ $num = 0;
     
   />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <script src="https://kit.fontawesome.com/961faa2e94.js" crossorigin="anonymous" data-auto-add-css="false"></script>
   <link
     rel="stylesheet"
     href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
   />
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<script src="https://kit.fontawesome.com/961faa2e94.js" crossorigin="anonymous" data-auto-add-css="false"></script>
+
   
   <link rel="stylesheet" href="../style2.css" />
   
