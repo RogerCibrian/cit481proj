1a7058d9f9da0be12713a11fa624f8ee84b2349c
mario-cit <71296920+mario-cit@users.noreply.github.com>
2021-04-27 23:40:13 -0700

    minor stuff

--- Playstation_games/7daystodie.php
+++ Playstation_games/7daystodie.php
@@ -47,7 +47,7 @@ $num = 0;
     rel="stylesheet"
     href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
   />
-<script type='text/javascript' src='../js/jquery-3.6.0.min.js'></script>
+<script type='text/javascript' src='../jquery-3.6.0.min.js'></script>
   <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> --> 
   
 <!---- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> ---->
