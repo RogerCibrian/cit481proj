3dd439c960d541f176f28def2cdef1db7561f587
RogerCibrian <70341939+RogerCibrian@users.noreply.github.com>
2021-04-27 21:23:57 -0700

    pls work

--- Playstation_games/7daystodie.php
+++ Playstation_games/7daystodie.php
@@ -36,10 +36,10 @@ $num = 0;
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>RottenPotatoes</title>
 	<link rel="stylesheet" href="/main.css" />
-  <script src="https://kit.fontawesome.com/961faa2e94.js" crossorigin="anonymous"></script>
+	<script src="./js/all.js" crossorigin="anonymous" data-auto-add-css="false"></script>
     <link
     rel="stylesheet"
-    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+    href="./css/fontawesome.min.css"
     
   />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
