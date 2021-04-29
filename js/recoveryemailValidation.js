ee81d9ed0870f8dd5149560134049fe63a558ba1
eneemz <erik.niemans@gmail.com>
2021-04-29 00:50:40 -0700

    fix button + submit via js

--- js/recoveryemailValidation.js
+++ js/recoveryemailValidation.js
@@ -1,19 +1,22 @@
-document.getElementById("recovery-form").addEventListener("click", emailValid);
+document.getElementById("recovery").addEventListener("click", emailValid);
 function emailValid(){
     var e = document.forms["myForm"]["mail"].value;
+    var email = document.getElementById('email');
+    var filter = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
 
     /* test if email input field is empty or not*/
     if (e == ""){
-        alert("Emaill Address field must be filled out");
+        alert("Email Address field must be filled out");
         return false;
     }
 
     /* makes sure if value enetred in email input field is in actual email format*/
-    var email = document.getElementById('email');
-    var filter = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if (!filter.test(email.value)){
+    else if (!filter.test(email.value)){
         alert("\nPlease provide a valid email address. Example: oinster159@sbcgobal.net");
             return false;
     }
-
+	
+	/* submit form */
+	else $('#recovery-form').submit();
 }
