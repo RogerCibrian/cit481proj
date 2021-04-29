801692daf50cdd8aab93addd299afd7929b1f511
eneemz <erik.niemans@gmail.com>
2021-04-29 00:54:10 -0700

    Change isset statement

--- pswd_recovery.php
+++ pswd_recovery.php
@@ -8,7 +8,7 @@ use PHPMailer\PHPMailer\Exception;
 
 require '/home/cit481/vendor/autoload.php';
 
-if (isset($_POST["resetrequestsubmit"])){
+if (isset($_POST["mail"])){
 	$selector = bin2hex(random_bytes(8));
 	$token = random_bytes(32);
 
