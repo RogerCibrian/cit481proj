9197c02af9f303446beba6f8725c1442c7630572
Jennifer Martinez <JenniferMartinez0715@yahoo.com>
2021-04-30 10:23:59 -0700

    removed unwanted codes

--- Playstation_games/Tekken5.php
+++ Playstation_games/Tekken5.php
@@ -36,8 +36,6 @@ $num = 0;
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>RottenPotatoes</title>
-
-  <meta name="viewport" content="width=device-width, initial-scale=1" />
   <link
     rel="stylesheet"
     href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
