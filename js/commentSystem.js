83a96d2b570f397556c368bb4cce218e5f5ecb9f
mario-cit <71296920+mario-cit@users.noreply.github.com>
2021-04-29 19:48:39 -0700

    k

--- js/commentSystem.js
+++ js/commentSystem.js
@@ -112,11 +112,11 @@ parent.addEventListener('click', event => {
     // for loop check if user already liked on that date
     for (let i = 0; i < lArray.length; i++){
      let a = lArray[i].date;
-     console.log(a);
+     //console.log(a);
      let b = lArray[i]["COUNT(likesvalue)"];
      //let c = lArray[i]["COUNT(dislikevalue)"];
      let dt1 = str3.trim();
-     console.log(dt1);
+    // console.log(dt1);
       if (dt1 === a){
        countn = 1;
       }
